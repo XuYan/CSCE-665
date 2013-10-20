767a5c618c0dfa420ff9ed7e8a43e524f5fd3bc2
Srinath N <irtmail-github@yahoo.in>
2013-10-20 12:39:58 -0500

    Uploading on behalf of Visva

---
+++ Extractor/functionExtractor.pl
@@ -0,0 +1,48 @@
+#!usr/bin/perl
+
+
+
+%myhash=();
+open FH1,"C:/Users/Visvanathan/Desktop/result.txt" or die $!;
+open FH2,">C:/Users/Visvanathan/Desktop/dictionary.txt" or die $!;
+#print <FH1>;
+foreach $line(<FH1>)
+{
+	
+	open FHnew,$line or die $!;
+	#print $_;
+	
+	foreach $newline(<FHnew>)
+	{
+				
+			if($newline=~/(\S+)\($/)
+			{
+				$_="class";
+				#if($newline=~/(class)(\s)(\S+)(\s{1})(.+)/)
+				#{
+					#print $1,"\n";
+					
+					foreach $keys(%myhash)
+					{
+					#	print $keys, " number 222=\n";
+						if($keys eq $1)
+						{
+							$count++;	
+						#	print "for" ,$1, " $count= ", $count, "\n";
+						}
+					}
+					$myhash{$1}=$count;
+						
+					print FH2 $1,": ",$count,"\n";			
+				#}
+				
+			}
+	}
+}
+$printline,$final;
+open FH3,"C:/Users/Visvanathan/Desktop/dictionary1.txt" or die $!;
+foreach $printline(<FH2>)
+{
+	print $printline, "\n";
+	if($final=~/
+}
